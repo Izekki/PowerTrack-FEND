036411a7ac400ac93a1d7a31a8c33e4046cf81a2
xib4lb4 <israel.romero.v@outlook.com>
2025-12-09 22:08:51 -0600

    Changes in Inicio and Consumo

--- src/components/HeaderPW.jsx
+++ src/components/HeaderPW.jsx
@@ -1,4 +1,5 @@
 // HeaderPW.jsx
+import React, { useState, useEffect } from "react";
 import "../styles/HeaderPW.css";
 import logo from "../assets/logo-pw.svg";
 import homeIcon from "../assets/sidebar-icons/home.png";
@@ -12,6 +13,7 @@ import configIcon from "../assets/sidebar-icons/ajuste.png";
 const getPageIcon = (pageTitle) => {
   const icons = {
     "Bienvenido": <img src={homeIcon} alt="Home" className="page-icon-img" />,
+    "Inicio": <img src={homeIcon} alt="Inicio" className="page-icon-img" />,
     "Consumo": <img src={consuIcon} alt="Consumo" className="page-icon-img" />,
     "Dispositivos": <img src={devicesIcon} alt="Dispositivos" className="page-icon-img" />,
     "Alertas": <img src={alertIcon} alt="Alertas" className="page-icon-img" />,
@@ -23,19 +25,36 @@ const getPageIcon = (pageTitle) => {
 };
 
 const HeaderPW = ({ userName, pageTitle = "Bienvenido" }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsCollapsed(true);
+    }, 3000);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
   const pageIcon = getPageIcon(pageTitle);
+  const displayTitle = isCollapsed && pageTitle === "Bienvenido" ? "Inicio" : pageTitle;
+  const displayText = isCollapsed 
+    ? displayTitle
+    : (pageTitle === "Bienvenido" ? `${pageTitle}, ${userName}` : pageTitle);
+
   return (
-    <header className="header-pw">
+    <header className={`header-pw ${isCollapsed ? "header-collapsed" : ""}`}>
       <div className="header-content">
-        <div className="logo-container">
-          <img src={logo} alt="Logo" className="logo" />
-        </div>
+        {!isCollapsed && (
+          <div className="logo-container">
+            <img src={logo} alt="Logo" className="logo" />
+          </div>
+        )}
 
         {/* Mostrar el título de la página con su icono */}
         <div className="user-info">
           <span className="user-name">
             {pageIcon && <span className="page-icon">{pageIcon}</span>}
-            {pageTitle === "Bienvenido" ? `${pageTitle}, ${userName}` : pageTitle}
+            {displayText}
           </span>
         </div>
       </div>
